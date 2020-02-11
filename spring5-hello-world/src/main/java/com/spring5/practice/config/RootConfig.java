0dc13303fdc165c841caf066d1714c8658ff5add
Syed Mainul Hasan <mainuls18@gmail.com>
2020-02-11 19:27:14 +0600

    spring5-hello-world

---
+++ spring5-hello-world/src/main/java/com/spring5/practice/config/RootConfig.java
@@ -0,0 +1,6 @@
+package com.spring5.practice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.spring5.practice.service"})
+public class RootConfig { }
---
+++ spring5-hello-world/src/main/java/com/spring5/practice/controllers/RootController.java
@@ -0,0 +1,22 @@
+package com.spring5.practice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String helloWorld(Model model) {
+        model.addAttribute("name", "World");
+        return "hello";
+    }
+
+    @GetMapping("/say-hello")
+    public String helloName(Model model, @RequestParam(name = "name", defaultValue = "") String name) {
+        model.addAttribute("name", name.isBlank() ? "World" : name);
+        return "hello";
+    }
+}
