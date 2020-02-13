8dc669a5179e039b7f9ae5d8aa7a4845b547c020
mainul35-bs23 <mainul.hasan@bs-23.net>
2020-02-13 08:25:09 +0600

    commiting static resources configuration

--- spring5-rendering-static-resources/src/main/java/com/spring5/practice/config/ServletConfig.java
+++ spring5-rendering-static-resources/src/main/java/com/spring5/practice/config/ServletConfig.java
@@ -3,6 +3,7 @@ package com.spring5.practice.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,29 @@ public class ServletConfig implements WebMvcConfigurer {
     // Configuration to render VIEWS
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    // Configuration to render STATIC CONTENTS (IMAGE, CSS, JS)
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // Register resource handler for -
+
+        // IMAGES
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/WEB-INF/resources/images/")
+                .setCachePeriod(999999999);
+
+        // CSS
+        registry
+                .addResourceHandler("/css/**")
+                .addResourceLocations("/WEB-INF/resources/css/")
+                .setCachePeriod(999999999);
+
+        // JAVASCRIPT
+        registry
+                .addResourceHandler("/js/**")
+                .addResourceLocations("/WEB-INF/resources/js/")
+                .setCachePeriod(999999999);
     }
 }
