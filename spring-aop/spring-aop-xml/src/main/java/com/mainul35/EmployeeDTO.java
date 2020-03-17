63759e793795ed679c3943c843e09b1932974e95
mainul35-bs23 <mainul.hasan@bs-23.net>
2020-03-17 13:38:29 +0600

    spring-aop java and xml config

---
+++ spring-aop/spring-aop-xml/src/main/java/com/mainul35/EmployeeDTO.java
@@ -0,0 +1,34 @@
+package com.mainul35;
+
+public class EmployeeDTO {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    //Setters and Getters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
