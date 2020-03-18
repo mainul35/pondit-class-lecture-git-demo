fe60e32e940e7ebb84622e7ad03f042b80d7ed98
mainul35-bs23 <mainul.hasan@bs-23.net>
2020-03-18 11:03:15 +0600

    spring boot demo

--- spring-boot-demo/src/main/java/com/springboot/demo/DemoApplication.java
+++ spring-boot-demo/src/main/java/com/springboot/demo/DemoApplication.java
@@ -2,9 +2,10 @@ package com.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
