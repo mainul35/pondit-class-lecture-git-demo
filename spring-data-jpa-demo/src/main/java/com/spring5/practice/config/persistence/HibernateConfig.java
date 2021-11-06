0bd959e7ce1ff1169240b09af3523d7a53c4bcbd
mainul35 <mainuls18@gmail.com>
2021-11-06 07:45:47 +0700

    HibernateConfig updated to work properly with Spring data jpa

--- spring-data-jpa-demo/src/main/java/com/spring5/practice/config/persistence/HibernateConfig.java
+++ spring-data-jpa-demo/src/main/java/com/spring5/practice/config/persistence/HibernateConfig.java
@@ -1,97 +1,77 @@
 package com.spring5.practice.config.persistence;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.reflections.Reflections;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.criteria.CriteriaBuilder;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Modifier;
 import java.util.Properties;
 
 @EnableJpaRepositories("com.spring5.practice.repositories")
+@PropertySource ("classpath:hibernate.properties")
 @org.springframework.context.annotation.Configuration
 public class HibernateConfig {
-    private SessionFactory sessionFactory = null;
 
-    private Session session;
+    @Value ("${hibernate.hbm2ddl.auto}")
+    private String HBM2DDL_AUTO;
+    @Value ("${hibernate.show_sql}")
+    private String HIBERNATE_SHOW_SQL;
+    @Value ("${hibernate.dialect}")
+    private String HIBERNATE_DIALECT;
+    @Value ("${hibernate.connection.password}")
+    private String DB_PASSWORD;
+    @Value ("${hibernate.connection.username}")
+    private String DB_USER;
+    @Value ("${hibernate.connection.url}")
+    private String DB_URL;
+    @Value ("${hibernate.connection.driver_class}")
+    private String DB_DRIVER_CLASS;
 
-    @Bean(value = "session")
-    public Session getSession() {
-        try{
-            session = createAndGetLocalSessionFactoryBean().getCurrentSession();
-        } catch(HibernateException e) {
-            e.printStackTrace();
-            System.out.println("Opening new session...");
-            session = createAndGetLocalSessionFactoryBean().openSession();
-        }
-        return session;
-    }
-
-    public CriteriaBuilder getCriteriaBuilder() {
-        Session session = getSession();
-        var tx = session.getTransaction();
-        if(!tx.isActive()) {
-        	tx = session.beginTransaction();
-        }
-        return session.getCriteriaBuilder();
-    }
-
-    public EntityManager entityManager() {
-        return createAndGetLocalSessionFactoryBean().createEntityManager();
+    @Bean(name = "dataSource")
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(DB_DRIVER_CLASS);
+        dataSource.setUrl(DB_URL);     //MySQL Specific: +"?createDatabaseIfNotExist=true&autoReconnect=true&useSSL=false"
+        dataSource.setUsername(DB_USER);
+        dataSource.setPassword(DB_PASSWORD);
+        return dataSource;
     }
 
     @Bean
-    public SessionFactory createAndGetLocalSessionFactoryBean() {
-        if (this.sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                // Hibernate settings equivalent to hibernate.cfg.xml's properties
-                Properties settings = getBuiltProperties("hibernate.properties");
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("com.spring5.practice.model");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", HIBERNATE_DIALECT);
+        hibernateProperties.put("hibernate.show_sql", HIBERNATE_SHOW_SQL);
+        hibernateProperties.put("hibernate.hbm2ddl.auto", HBM2DDL_AUTO);
+        hibernateProperties.put("hibernate.connection.autocommit", false);
+        hibernateProperties.put("hibernate.enable_lazy_load_no_trans", true);
+        sessionFactoryBean.setHibernateProperties(hibernateProperties);
 
-                configuration.setProperties(settings);
-                configuration.addPackage("com.spring5.practice.model");
-                for (Class<?> clazz : (new Reflections("com.spring5.practice.model")).getTypesAnnotatedWith(Entity.class)) {
-                    if (!Modifier.isAbstract(clazz.getModifiers())) {
-                        configuration.addAnnotatedClass(clazz);
-                    }
-                }
-                StandardServiceRegistryBuilder serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(settings);
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry.build());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return sessionFactory;
-    }
-
-    protected Properties getBuiltProperties(String propertyFileName) {
-        Properties properties = new Properties();
-        InputStream input = HibernateConfig.class
-                .getClassLoader().getResourceAsStream(propertyFileName);
-        try {
-            properties.load(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return properties;
+        return sessionFactoryBean;
     }
 
     @Bean
-    public EntityManagerFactory entityManagerFactory(Session session) {
-        return session.getEntityManagerFactory();
+    public EntityManagerFactory entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.spring5.practice.model");
+        factory.setDataSource(dataSource());
+        factory.afterPropertiesSet();
+
+        return factory.getObject();
     }
 
     @Bean("transactionManager")
